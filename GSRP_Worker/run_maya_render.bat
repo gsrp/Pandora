b6b36ef08cf056bcb980ff1a14111ae6f931c1ae
shiqinfeng1 <150627601@qq.com>
2019-08-23 12:57:33 +0800

    update

--- GSRP_Worker/run_maya_render.bat
+++ GSRP_Worker/run_maya_render.bat
@@ -12,7 +12,7 @@ set dangqian_lujing=%cd%
 
 :: 空闲时定时检查是否有渲染任务
 :tryFetch
-set wait=60
+set wait=20
 @echo ++++
 @echo [STEP 0 tryFetch] Try Maya Reander . Wait %wait% s ...
 @echo ++++
@@ -23,34 +23,29 @@ call :checkAndRun
 
 :checkAndRun
 @echo [STEP 1 checkAndRun] Get Maya File Path : %work_path%
-call :execcmd 
-ping -n 50 127.0.0.1>nul
-goto waitWork
+if exist %work_path%\rendercmd.txt (
+	call :execcmd 
+) else (
+	@echo [STEP 2.1 execcmd] Not Exsist CmdFile: %work_path%\rendercmd.txt
+	goto tryFetch
+)
 
 
 :: 执行文本中的命令     
 :execcmd
-if exist %work_path%\rendercmd.txt (
-	@echo ###########################
-	@echo [STEP 2 execcmd] Start Render. CmdFile: %work_path%\rendercmd.txt
-	@echo ###########################
-	for /f "delims=" %%i in (%work_path%\rendercmd.txt) do (
-		@echo EXEC Cmd:  %%i
-		start %%i
-	)
-) else (
-	@echo [STEP 2.1 execcmd] Not Exsist CmdFile: %work_path%\rendercmd.txt
+@echo ###########################
+@echo [STEP 2 execcmd] Start Render. CmdFile: %work_path%\rendercmd.txt
+@echo ###########################
+for /f "delims=" %%i in (%work_path%\rendercmd.txt) do (
+	@echo EXEC Cmd:  %%i
+	start %%i
+	ping -n 120 127.0.0.1>nul
+	goto waitWork
 )
 goto:eof
 
-
 :: 等待任务完成
 :waitWork
-
-if not exist %work_path%\rendercmd.txt (
-	@echo [STEP 3.1 waitWork] Not Exsist CmdFile: %work_path%\rendercmd.txt
-	goto tryFetch
-)
 @echo ###########################
 @echo [STEP 3 waitWork] Wait work to finish ...
 @echo ###########################
